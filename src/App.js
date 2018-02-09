7cac66bc10191344879efe9fd0ede7f945be4de6
fangtasy <jinxinf@mtu.edu>
2018-02-09 14:42:05 -0500

    react-redux added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Auth from './auth'
-import logo from './logo.svg';
+import Auth from './component/auth'
 import './App.css';
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware,compose} from'redux';
+import {BrowserRouter,Route,Redirect,Switch} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initState={
+	flag: 'failed'
+}
+
+function reducer(state=initState,action){
+	switch(action.type){
+		case 'Login_Success':
+			return {...state, flag:'succeed'};
+		case 'Login_Failed':
+			return {...state, flag:'failed'};
+		default: return initState;
+	}
+}
+const store= createStore(reducer,compose(applyMiddleware(thunk),window.devToolsExtension?window.devToolsExtension():()=>{}));
+store.dispatch({type:'Login_Success'});
+console.log(store.getState());
+ReactDOM.render(
+	<Provider store={store}>
+	<App />
+	</Provider>, document.getElementById('root'));
 registerServiceWorker();
